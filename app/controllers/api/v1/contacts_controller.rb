828c2d79c18f8f040f53e6b54e20b6ecaf247bf4
cdorella <caroldorella@gmail.com>
2020-09-21 16:43:58 +0200

    refactor: optimization changes to controller, seed and client files

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -3,36 +3,36 @@ module Api
     class ContactsController < ApplicationController
       def index
         contacts = Contact.order('first_name ASC')
-        render json: { status: 'Success', message: 'Contacts loaded correctly', data: contacts }, status: :ok
+        render json: { status: 'Success', data: contacts }, status: :ok
       end
 
       def show
         contact = Contact.find(params[:id])
-        render json: { status: 'Success', message: 'Contact loaded correctly', data: contact }, status: :ok
+        render json: { status: 'Success', data: contact }, status: :ok
       end
 
       def create
         contact = Contact.new(contact_params)
         if contact.save
-          render json: { status: 'Success', message: 'Contact saved in the database', data: contact }, status: :ok
+          render json: { status: 'Success', data: contact }, status: :ok
         else
-          render json: { status: 'Error', message: 'Contact not saved in the database', data: contact.errors }, status: :unprocessable_entity
+          render json: { status: 'Error', data: contact.errors }, status: :unprocessable_entity
         end
       end
 
       def update
         contact = Contact.find(params[:id])
         if contact.update(contact_params)
-          render json: { status: 'Success', message: 'Contact updated in the database', data: contact }, status: :ok
+          render json: { status: 'Success', data: contact }, status: :ok
         else
-          render json: { status: 'Error', message: 'Contact not updatedin the database', data: contact.errors }, status: :unprocessable_entity
+          render json: { status: 'Error', data: contact.errors }, status: :unprocessable_entity
         end
       end
 
       def destroy
         contact = Contact.find(params[:id])
         contact.destroy
-        render json: { status: 'Success', message: 'Contact deleted from database', data: contact }, status: :ok
+        render json: { status: 'Success', data: contact }, status: :ok
       end
 
       private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
 Contact.destroy_all
 
 Contact.create([{
-                 first_name: 'Jack',
-                 last_name: 'Johnson',
-                 email: 'jj@email.com',
-                 phone_number: '699 890 453'
+                 first_name: 'Eddie',
+                 last_name: 'Vedder',
+                 email: 'pearljam@email.com',
+                 phone_number: '622 123 999'
                },
+                {
+                  first_name: 'Jack',
+                  last_name: 'Johnson',
+                  email: 'hawaiian@email.com',
+                  phone_number: '699 890 453'
+                },
                 {
                   first_name: 'Florence',
                   last_name: 'Welch',
-                  email: 'flo@email.com',
+                  email: 'florenceandthemachine@email.com',
                   phone_number: '633 555 443'
                 },
                 {
                   first_name: 'Lana',
                   last_name: 'Del Rey',
-                  email: 'lana@email.com',
+                  email: 'videogames@email.com',
                   phone_number: '611 234 909'
                 }])
 
